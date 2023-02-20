d97e1f013b679b2450a0472b61fe9a9b6136c700
RafaGit707 <rgalcal303@g.educaand.es>
2023-02-20 14:34:47 +0100

    .

---
+++ Java/Cuadrado.java
@@ -0,0 +1,15 @@
+class Cuadrado {
+
+   int lado;
+   String color;
+
+   //CREAMOS LOS CONSTRUCTORES
+
+   public Cuadrado(int lado, String color)
+   
+   {
+      this.lado = lado;
+      this.color = color;
+   }
+   
+}
