2dfd957c64f18bbfb6b7d373f3a42169423afeab
RafaGit707 <rgalcal303@g.educaand.es>
2023-02-22 14:35:13 +0100

    .

---
+++ Java/FrioMIjas/Jefe.java
@@ -0,0 +1,21 @@
+public class Jefe {
+
+   String nombre;
+   String apellido;
+   int edad;
+   int salario;
+   String dni;
+
+   //CREAMOS LOS CONSTRUCTORES
+
+   public Jefe(String nombre, String apellido, int edad, int salario, String dni)
+   {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.salario = salario;
+      this.dni = dni;
+   }
+
+
+}
---
+++ Java/FrioMIjas/Sede.java
@@ -0,0 +1,20 @@
+public class Sede {
+
+      String provinicia;
+      String calle;
+      int codPostal;
+      String jefe;
+   
+      //CREAMOS LOS CONSTRUCTORES
+   
+      public Sede(String provincia, String calle, int codPostal, String jefe)
+      {
+         this.provinicia = provincia;
+         this.calle = calle;
+         this.codPostal = codPostal;
+      }
+      void setJefe(String jefe)
+      {
+         setJefe(jefe);
+      }
+}
