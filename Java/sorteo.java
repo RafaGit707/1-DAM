80afcb1dc3964348479428d47a1a64293ac44f31
RafaGit707 <117975850+RafaGit707@users.noreply.github.com>
2023-03-22 12:48:50 +0000

    .

---
+++ Java/sorteo.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class sorteo {
+    
+    private HashSet<T> bolsa;
+
+    public Sorteo()
+    {
+        bolsa = new HashSet<T>();
+    }
+    public boolean add(T elemento)
+    {
+        return bolsa.add(elemento);
+    }
+    public Set<T> premiados (int numPremiados)
+    {
+        ArrayList<T> premiados = new ArrayList<T>();
+    }
+
+}
